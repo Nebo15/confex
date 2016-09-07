e9d0e6c3f1ce1f01badd20e4fff68feafb494cb7
Andrew Dryga <andrew@dryga.com>
2016-09-07 12:53:03 +0300

    Full code coverage

--- test/unit/confex_test.exs
+++ test/unit/confex_test.exs
@@ -2,7 +2,55 @@ defmodule ConfexTest do
   use ExUnit.Case
   doctest Confex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "use nil maps" do
+    assert nil == Confex.get_map(:myapp, :missing_map)
+  end
+
+  test "different definition types" do
+    Application.put_env(:confex, __MODULE__, [
+       foo: "bar",
+       num: 1,
+       baz: {:system, "TESTENV"},
+       biz: {:system, "TESTENV", "default_val"},
+       mex: {:system, :string, "TESTENV"},
+       tox: {:system, :string, "TESTENV", "default_val"},
+    ])
+
+    assert [foo: "bar",
+            num: 1,
+            baz: nil,
+            biz: "default_val",
+            mex: nil,
+            tox: "default_val"] = Confex.get_map(:confex, __MODULE__)
+
+    System.put_env("TESTENV", "other_val")
+
+    assert [foo: "bar",
+            num: 1,
+            baz: "other_val",
+            biz: "other_val",
+            mex: "other_val",
+            tox: "other_val"] = Confex.get_map(:confex, __MODULE__)
+  end
+
+  test "integer fields" do
+    Application.put_env(:confex, __MODULE__, [
+       foo: "bar",
+       num: 1,
+       mex: {:system, :integer, "TESTINTENV"},
+       tox: {:system, :integer, "TESTINTENV", 300},
+    ])
+
+    assert [foo: "bar",
+            num: 1,
+            mex: nil,
+            tox: 300] = Confex.get_map(:confex, __MODULE__)
+
+    System.put_env("TESTINTENV", "600")
+
+    assert [foo: "bar",
+            num: 1,
+            mex: 600,
+            tox: 600] = Confex.get_map(:confex, __MODULE__)
   end
 end
