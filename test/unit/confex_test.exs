3bf8ddd24ab8b1cc8592431ccb8b60f8a19adfb9
Eugene Pirogov <iamexile@gmail.com>
2016-11-02 23:39:17 +0200

    Add support for more types (#3)
    * Support atom types
    
    * Support module type
    
    * Increment version

--- test/unit/confex_test.exs
+++ test/unit/confex_test.exs
@@ -125,6 +125,26 @@ defmodule ConfexTest do
             d: "env_val"] = Confex.get_map(:confex, __MODULE__)
   end
 
+  test "sets atoms" do
+    System.put_env("TESTENV", "my_symbol")
+
+    Application.put_env(:confex, __MODULE__, [
+       a: {:system, :atom, "TESTENV"},
+    ])
+
+    assert [a: :my_symbol] = Confex.get_map(:confex, __MODULE__)
+  end
+
+  test "sets modules" do
+    System.put_env("TESTENV", "MyModule")
+
+    Application.put_env(:confex, __MODULE__, [
+       a: {:system, :module, "TESTENV"},
+    ])
+
+    assert [a: MyModule] = Confex.get_map(:confex, __MODULE__)
+  end
+
   test "walks on nested maps" do
     Application.put_env(:confex, __MODULE__, [
        a: [aa: "bar",
