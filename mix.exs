9c3d596c5167add51bdebe7e12688b32b354efc6
Andrew Dryga <andrew@dryga.com>
2017-03-22 20:34:08 +0200

    Drop unused stuff from mix.exs

--- mix.exs
+++ mix.exs
@@ -23,12 +23,12 @@ defmodule Confex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   # Dependencies can be Hex packages:
   #
