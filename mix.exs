7015d3e6c9bf3dbf01fedeeda4052be4daa83683
Andrew Dryga <andrew@dryga.com>
2017-06-30 12:03:16 +0300

    Bump minimum deps requirements

--- mix.exs
+++ mix.exs
@@ -44,10 +44,10 @@ defmodule Confex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, ">= 0.0.0",      only: [:dev, :test]},
-     {:excoveralls, ">= 0.5.0", only: [:dev, :test]},
+    [{:ex_doc, ">= 0.16.0",     only: [:dev, :test]},
+     {:excoveralls, ">= 0.7.0", only: [:dev, :test]},
      {:dogma, "> 0.1.0",        only: [:dev, :test]},
-     {:credo, ">= 0.4.8",       only: [:dev, :test]}]
+     {:credo, ">= 0.8.0",       only: [:dev, :test]}]
   end
 
   defp description do
