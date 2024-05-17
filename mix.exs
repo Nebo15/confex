9b9ef44a5241efd886acb3deb99a9396c493bd63
Andrew Dryga <andrew@dryga.com>
2024-05-17 12:38:33 -0600

    Re-enable CI

--- mix.exs
+++ mix.exs
@@ -10,20 +10,22 @@ defmodule Confex.Mixfile do
       description: description(),
       package: package(),
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test],
-      docs: [source_url: @source_url, source_ref: "v#\{@version\}", main: "readme", extras: ["README.md"]]
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
   def application do
-    [applications: []]
+    [
+      extra_applications: []
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -33,9 +35,21 @@ defmodule Confex.Mixfile do
     [
       {:ex_doc, "~> 0.19", only: :dev},
       {:excoveralls, ">= 0.7.0", only: [:dev, :test]},
-      {:credo, ">= 0.8.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
-      {:inch_ex, ">= 0.0.0", only: :docs}
+      {:dialyxir, "~> 1.4", only: [:dev]}
+    ]
+  end
+
+  defp package do
+    [
+      description: description(),
+      contributors: ["Andrew Dryga"],
+      maintainers: ["Andrew Dryga"],
+      licenses: ["MIT"],
+      files: ~w(lib LICENSE.md mix.exs README.md),
+      links: %{
+        Changelog: "#{@source_url}/blob/master/CHANGELOG.md}",
+        GitHub: @source_url
+      }
     ]
   end
 
@@ -46,13 +60,16 @@ defmodule Confex.Mixfile do
     """
   end
 
-  defp package do
+  defp docs do
     [
-      contributors: ["Andrew Dryga"],
-      maintainers: ["Andrew Dryga"],
-      licenses: ["LICENSE.md"],
-      links: %{Changelog: "#{@source_url}/blob/master/CHANGELOG.md}", GitHub: @source_url},
-      files: ~w(lib LICENSE.md mix.exs README.md)
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version,
+      formatters: ["html"]
     ]
   end
 end
